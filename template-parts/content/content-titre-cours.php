7f5405ade24fe21d7225dad6fdcbd0ba9d3b8162
Phil <philippe.thibeault99@outlook.com>
2019-03-05 10:38:41 -0500

    tri par session avec couleurs

--- template-parts/content/content-titre-cours.php
+++ template-parts/content/content-titre-cours.php
@@ -10,7 +10,12 @@
  */
 
 ?>
-<div class="box">
+<div class="box session<?php 
+	$session = "";
+	$post_slug = get_post_field( 'post_name', get_post () );
+	$session = substr($post_slug, -3,1);
+	echo $session;
+	?>">
 <article id="post-<?php the_ID();?>" class="cours" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php
